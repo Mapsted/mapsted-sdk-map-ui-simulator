f078a86f3748a3cff7928a7873a8c2b070dec9b4
Parth Bhatt <parth.bhatt@mapsted.com>
2023-10-05 10:04:18 +0530

    - Added v5.2.2 simulator SDK

---
+++ MapstedComponentsCore.framework/Headers/MapstedComponentsCore.h
@@ -0,0 +1,18 @@
+//
+//  MapstedComponentsCore.h
+//  MapstedComponentsCore
+//
+//  Created by Daniel on 2021-06-04.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MapstedComponentsCore.
+FOUNDATION_EXPORT double MapstedComponentsCoreVersionNumber;
+
+//! Project version string for MapstedComponentsCore.
+FOUNDATION_EXPORT const unsigned char MapstedComponentsCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapstedComponentsCore/PublicHeader.h>
+
+
---
+++ MapstedComponentsUI.framework/Headers/MapstedComponentsUI.h
@@ -0,0 +1,18 @@
+//
+//  MapstedComponentsUI.h
+//  MapstedComponentsUI
+//
+//  Created by Daniel on 2021-06-08.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MapstedComponentsUI.
+FOUNDATION_EXPORT double MapstedComponentsUIVersionNumber;
+
+//! Project version string for MapstedComponentsUI.
+FOUNDATION_EXPORT const unsigned char MapstedComponentsUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapstedComponentsUI/PublicHeader.h>
+
+
