4c1b641a0589eab02635fdc808594f865e25ca52
Amisha.Beladiya <amisha.beladiya@mapsted.com>
2026-01-09 10:43:18 +0530

    Update to SDK version v6.2.9

--- MapstedMapUi.framework/Headers/MapstedMapUi-Swift.h
+++ MapstedMapUi.framework/Headers/MapstedMapUi-Swift.h
@@ -704,10 +704,10 @@ SWIFT_CLASS("_TtC12MapstedMapUi34MapstedMapUiInternalViewController")
 
 
 
-
 @interface MapstedMapUiInternalViewController (SWIFT_EXTENSION(MapstedMapUi)) <ValidBlueDotCallbackListener>
 - (void)onValidBlueDotStateChangeWithError:(EnumValidBlueDotError)error;
 @end
+
 
 
 
@@ -1799,10 +1799,10 @@ SWIFT_CLASS("_TtC12MapstedMapUi34MapstedMapUiInternalViewController")
 
 
 
-
 @interface MapstedMapUiInternalViewController (SWIFT_EXTENSION(MapstedMapUi)) <ValidBlueDotCallbackListener>
 - (void)onValidBlueDotStateChangeWithError:(EnumValidBlueDotError)error;
 @end
+
 
 
 
